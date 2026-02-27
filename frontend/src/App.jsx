66ac9279a43189d6027bac83d8ec88db97901a9f
Sameer-Beg <sameer423j@gmail.com>
2026-02-27 14:48:32 +0530

    fisrt commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-red-600 font-blod bg-green-300'>sameer is developer post in UAE </h1>
+    </div>
+  )
+}
+
+export default App

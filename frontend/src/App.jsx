6b713d1374325da0831bfab91c6820dc7b9d1e8a
Sameer-Beg <sameer423j@gmail.com>
2026-02-27 14:52:31 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
       <h1 className='text-red-600 font-blod bg-green-300'>sameer is developer post in UAE </h1>
+      <h2 className='text-black bg-white text-4xl '>now he is working in the indian railways as a train manager </h2>
     </div>
   )
 }
